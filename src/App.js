f8f91bb20a974bf1e8d7fb7601731a170078943b
n3vo <nevo.riemer@gmail.com>
2022-05-07 01:37:10 +0300

    d

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import { Login } from "./Login";
 import { Step1 } from "./Step1";
 import { Step2 } from "./Step2";
